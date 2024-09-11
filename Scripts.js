9d32a3ed9f17bb1b87a2a55338bf408cdd8db2dd
jaliecac <jalieca.culhane@gmail.com>
2024-09-11 14:22:00 +1000

    Wrote some javascript functions

--- Scripts.js
+++ Scripts.js
@@ -1,6 +1,24 @@
+function calculateResults() {
+    let rate = document.getElementById("rate").value;
+    let term = document.getElementById("term").value;
+    let mortgage = document.getElementById("amount").value;
+    let totalPayment = mortgage;
 
-function selectChange() {
-    const sample = document.getElementById("myid"); // using const
-    // change css style
-    sample.style.color = 'red';
+    for (let i=1; i<term; i++) {
+        totalPayment = parseInt(totalPayment) + (parseInt(totalPayment)*(parseInt(rate)/100))
+    }
+    document.getElementById("totalRepay").value = "$" + totalPayment;
+}
+
+function clearAll() {
+    document.getElementById("amount").value = "";
+    document.getElementById("term").value = "";
+    document.getElementById("rate").value = "";
+
+    let radios = document.getElementsByTagName("input");
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].type == "radio") {
+            radios[i].checked = false;
+        }
+    }
 }
